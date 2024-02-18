741d594e979e7ab2dda260a6eb4dc09041e3220f
yyoshidaweb <yoshidayumajob@gmail.com>
2024-02-18 17:10:58 +0900

    アーティストデータを全件取得する機能を追加

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Artist extends Model
 {
@@ -13,4 +14,10 @@ class Artist extends Model
     protected $fillable = [
         'name',
     ];
+
+    // ArtistからUserへのリレーション
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
