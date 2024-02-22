28e7ad037d5dfbca756840dc0b45d761adae8123
yyoshidaweb <yoshidayumajob@gmail.com>
2024-02-22 18:36:58 +0900

    add: 編集ページ表示用のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController; //アーティストコントローラー
+use Faker\Guesser\Name;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,12 +35,17 @@ Route::middleware('auth')->group(function () {
  */
 // 未ログインユーザー用
 Route::controller(ArtistController::class)->group(function () {
+    // アーティストリストを表示する
     Route::get('/artists', 'index')->name('artists.index');
 });
 
 // 認証済ユーザー用
 Route::controller(ArtistController::class)->middleware(['auth', 'verified'])->group(function () {
+    // アーティストリスト編集画面を表示する
+    Route::get('/artists/edit', 'edit')->name('artists.edit');
+    // アーティスト追加を実行する
     Route::post('/artists', 'store')->name('artists.store');
+    // アーティスト削除を実行する
     Route::delete('/artists/{artist}', 'destroy')->name('artists.destroy');
 });
 
