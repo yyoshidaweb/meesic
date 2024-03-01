5f80ae8e35d6fe4195cd94b8b1bee202e8755cb2
yyoshidaweb <yoshidayumajob@gmail.com>
2024-03-01 22:36:52 +0900

    feat: artistsテーブルを作成するためのマイグレーションファイルから、user_idカラムを削除

--- database/migrations/2024_02_14_025457_create_artists_table.php
+++ database/migrations/2024_02_14_025457_create_artists_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('name', 100);
             $table->timestamps();
         });
