5e80bb7334c7ed20c00e6e2421a77d47e1ab9699
yyoshidaweb <yoshidayumajob@gmail.com>
2024-03-08 18:13:16 +0900

    add: SpotifyArtistモデル、コントローラー、マイグレーションファイルを作成

---
+++ app/Http/Controllers/SpotifyArtistController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpotifyArtist;
+use Illuminate\Http\Request;
+
+class SpotifyArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SpotifyArtist $spotifyArtist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SpotifyArtist $spotifyArtist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SpotifyArtist $spotifyArtist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SpotifyArtist $spotifyArtist)
+    {
+        //
+    }
+}
