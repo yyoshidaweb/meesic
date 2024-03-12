55ef6f2e2c09ae6ccb70e85b749a24c6ae95dc2d
yyoshidaweb <yoshidayumajob@gmail.com>
2024-03-12 18:12:12 +0900

    add: SpotifyArtistController内に、Spotify APIのアクセストークンを取得するための、getAccessTokenメソッドを追加

--- app/Http/Controllers/SpotifyArtistController.php
+++ app/Http/Controllers/SpotifyArtistController.php
@@ -3,63 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\SpotifyArtist;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class SpotifyArtistController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Spotify APIのアクセストークンを取得し、暗号化し、sessionに保存する。
+     *
+     * @return void
      */
-    public function index()
+    public function getAccessToken()
     {
-        //
-    }
+        // リクエストパラメータ
+        $params = [
+            'grant_type' => 'client_credentials',
+        ];
+        // Guzzleクライアントを作成
+        $client = new \GuzzleHttp\Client();
+        // GET通信するURL
+        $url = 'https://accounts.spotify.com/api/token';
+        // Spotify Client ID
+        $client_id = config('services.spotify.client_id');
+        // Spotify Client secret
+        $client_secret = config('services.spotify.client_secret');
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        // リクエスト
+        $response = $client->request('POST', $url, [
+            'form_params' => $params,
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode($client_id . ':' . $client_secret),
+            ],
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(SpotifyArtist $spotifyArtist)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(SpotifyArtist $spotifyArtist)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, SpotifyArtist $spotifyArtist)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(SpotifyArtist $spotifyArtist)
-    {
-        //
+        // API通信で取得したデータはjson形式なのでPHPファイルに対応した連想配列にデコードする
+        $data = json_decode($response->getBody()->getContents(), true);
+        // 取得したデータからアクセストークンを取得
+        $access_token = $data['access_token'];
+        // アクセストークンを暗号化
+        $encrypted_access_token = Crypt::encryptString($access_token);
+        // 暗号化済みアクセストークンをsessionに保存
+        session(['access_token' => $encrypted_access_token]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController; //アーティストコントローラー
+use App\Http\Controllers\SpotifyArtistController;
 use Faker\Guesser\Name;
 
 /*
@@ -28,6 +29,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/**
+ * SpotifyArtistControllerのルーティンググループ
+ */
+Route::controller(SpotifyArtistController::class)->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/refresh-token', 'getAccessToken')->name('spotify.getAccessToken');
 });
 
 /**
