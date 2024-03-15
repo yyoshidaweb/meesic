6699c14b4a28fb464de36391a8d560eea3c446be
yyoshidaweb <yoshidayumajob@gmail.com>
2024-03-15 19:52:39 +0900

    style: edit-artists.blade.phpの見た目を変更

--- resources/views/artists/edit-artists.blade.php
+++ resources/views/artists/edit-artists.blade.php
@@ -1,6 +1,10 @@
 <x-app-layout>
+    {{-- Spotifyアーティスト --}}
+    <div class="mt-4 mb-4">
+        <h2 class="text-2xl">Spotifyアーティストを追加</h2>
+    </div>
     {{-- アーティスト検索フォーム全体 --}}
-    <div class="">
+    <div class="mb-4">
         {{-- POSTリクエストでsearchArtistsメソッドを使用 --}}
         <form method="POST" action="{{ route('artists.searchArtists') }}">
             {{-- CSRF保護 --}}
@@ -23,15 +27,15 @@
     </div>
 
     {{-- 検索結果を表示 --}}
-    <div class="">
+    <div class="mb-10">
         {{-- 検索結果が存在する場合に表示 --}}
         @isset($result_artists)
-            <p class="">検索結果:</p>
+            <h3 class="text-xl mb-4">検索結果</h3>
             {{-- 検索結果一覧を表示 --}}
             @foreach ($result_artists as $result_artist)
                 <div class="flex mb-2">
                     {{-- アーティスト名 --}}
-                    <p class="mr-4">{{ $result_artist['name'] }}</p>
+                    <p class="mr-3">{{ $result_artist['name'] }}</p>
                     {{-- spotify_idを保存するフォーム --}}
                     <form method="POST" action="{{ route('spotify.storeSpotifyId') }}">
                         {{-- CSRF保護 --}}
@@ -48,15 +52,42 @@
         @endisset
     </div>
 
+    {{-- Spotifyアーティスト一覧を表示 --}}
+    <div class="mb-10">
+        {{-- Spotify_artistsが存在する場合 --}}
+        @isset($spotify_artists)
+            {{-- Spotify上のアーティストを表示 --}}
+            @foreach ($spotify_artists as $spotify_artist)
+                <div class="flex">
+                    <div class="mb-4 mr-2">
+                        {{-- アーティスト名 --}}
+                        <p class="">{{ $spotify_artist['name'] }}</p>
+                    </div>
+                    {{-- 削除 --}}
+                    <form method="POST" action="{{ route('spotify.detach', $spotify_artist['id']) }}">
+                        @csrf
+                        @method('delete')
+                        <x-primary-button
+                            type="submit"
+                            class="bg-red-500"
+                            onclick="return confirm('本当に削除しますか？')"
+                        >削除</x-primary-button>
+                    </form>
+                </div>
+            @endforeach
+        @endisset
+    </div>
+
+    {{-- カスタムアーティスト --}}
+    <h2 class="text-2xl">カスタムアーティストを追加</h2>
     {{-- アーティスト追加フォーム全体 --}}
-    <div class="">
+    <div class="mb-4">
         {{-- POSTリクエストでaddメソッドを使用 --}}
         <form method="POST" action="{{ route('artists.add') }}">
             {{-- CSRF保護 --}}
             @csrf
             {{-- 入力エリア --}}
             <label>
-                アーティスト名
                 <input
                     type="text"
                     name="name"
@@ -67,50 +98,31 @@
                 {{-- エラーメッセージを表示する --}}
                 <x-input-error :messages="$errors->get('name')" class="" />
                 {{-- 送信ボタン --}}
-                <x-primary-button class="">アーティストを追加</x-primary-button>
+                <x-primary-button class="">リストに追加</x-primary-button>
             </label>
         </form>
     </div>
 
-    {{-- アーティスト一覧を表示 --}}
-    <div class="">
-        {{-- Spotify_artistsが存在する場合 --}}
-        @isset($spotify_artists)
-            {{-- Spotify上のアーティストを表示 --}}
-            @foreach ($spotify_artists as $spotify_artist)
-                <div class="">
-                    {{-- アーティスト名 --}}
-                    <p class="">{{ $spotify_artist['name'] }}</p>
-                </div>
-                {{-- 削除 --}}
-                <form method="POST" action="{{ route('spotify.detach', $spotify_artist['id']) }}">
-                    @csrf
-                    @method('delete')
-                    <x-primary-button
-                        type="submit"
-                        class="bg-red-500"
-                        onclick="return confirm('本当に削除しますか？')"
-                    >削除</x-primary-button>
-                </form>
-            @endforeach
-        @endisset
-
+    {{-- カスタムアーティスト一覧を表示 --}}
+    <div>
         @foreach ($artists as $artist)
-            <div class="">
-                <p class="">{{ $artist->name }}</p>
+            <div class="flex">
+                <div class="mb-4 mr-2">
+                    <p class="">{{ $artist->name }}</p>
+                </div>
+                {{-- もし削除権限がある場合に表示する --}}
+                @if ($artist->users->contains(auth()->user()))
+                    <form method="POST" action="{{ route('artists.detach', $artist) }}">
+                        @csrf
+                        @method('delete')
+                        <x-primary-button
+                            type="submit"
+                            class="bg-red-500"
+                            onclick="return confirm('本当に削除しますか？')"
+                        >削除</x-primary-button>
+                    </form>
+                @endif
             </div>
-            {{-- もし削除権限がある場合に表示する --}}
-            @if ($artist->users->contains(auth()->user()))
-                <form method="POST" action="{{ route('artists.detach', $artist) }}">
-                    @csrf
-                    @method('delete')
-                    <x-primary-button
-                        type="submit"
-                        class="bg-red-500"
-                        onclick="return confirm('本当に削除しますか？')"
-                    >削除</x-primary-button>
-                </form>
-            @endif
         @endforeach
         {{-- ページネーション --}}
         <div class="paginate">{{ $artists->links() }}</div>
