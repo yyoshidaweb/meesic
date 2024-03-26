75939fe6a21bf37f76106ee5eb43de52c4c17c25
yyoshidaweb <yoshidayumajob@gmail.com>
2024-03-26 19:16:29 +0900

    update: 公開用ページのカスタムアーティストのデザインを変更

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -20,13 +20,20 @@
                     @endforeach
                 </div>
             @endisset
-            @foreach ($artists as $artist)
-                <div class="">
-                    <p class="">{{ $artist->name }}</p>
+            {{-- カスタムアーティストが存在する場合 --}}
+            @isset($artists)
+                <div class="w-full">
+                    @foreach ($artists as $artist)
+                        <div class="mt-4 w-full scale-100 bg-white p-6 dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-md dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250">
+                            <div class="flex w-full items-center justify-center">
+                                <p class="font-semibold text-gray-900 dark:text-white">{{ $artist->name }}</p>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
-            {{-- ページネーション --}}
-            <div class="paginate">{{ $artists->links() }}</div>
+                {{-- ページネーション --}}
+                <div class="paginate">{{ $artists->links() }}</div>
+            @endisset
         </div>
     </div>
 </x-app-layout>
