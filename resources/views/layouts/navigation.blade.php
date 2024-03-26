af7242fd9ce1470dc43091c5329cc583fa7e5371
yyoshidaweb <yoshidayumajob@gmail.com>
2024-03-26 17:18:30 +0900

    update: モバイル用のナビゲーションにもログインと新規登録のリンクを追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -86,17 +86,15 @@
 
             {{-- ここからモバイル向けナビゲーション --}}
 
-            {{-- ハンバーガーメニュー(ログイン済ユーザーのみに表示) --}}
-            @auth
-                <div class="-me-2 flex items-center sm:hidden">
-                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-            @endauth
+            {{-- ハンバーガーメニュー(全てのユーザーのみに表示) --}}
+            <div class="-me-2 flex items-center sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
         </div>
     </div>
 
@@ -140,6 +138,20 @@
                         </x-responsive-nav-link>
                     </form>
                 </div>
+            </div>
+        </div>
+    @else
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                {{-- ログイン --}}
+                <x-responsive-nav-link :href="route('login')">
+                    ログイン
+                </x-responsive-nav-link>
+                {{-- 新規登録 --}}
+                <x-responsive-nav-link :href="route('register')">
+                    新規登録
+                </x-responsive-nav-link>
+                {{-- アーティストリスト編集ページへのリンク --}}
             </div>
         </div>
     @endauth
