269dc3eb7e77ea7a3aa3478ae9dcc5b6ecf41575
yyoshidaweb <yoshidayumajob@gmail.com>
2024-04-05 03:46:22 +0900

    update: トップページのログイン用の各ボタンを未ログイン時のみ表示する仕様に変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,30 +11,32 @@
                 </a>
             </div>
             {{-- 各ボタン全体 --}}
-            <div class="mt-16">
-                <div class="flex flex-col items-center">
-                    {{-- 新規登録ボタン --}}
-                    <a href="{{ route('register') }}" class="w-full scale-100 bg-white p-6 dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-md dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250">
-                        <div class="flex w-full items-center justify-center">
-                            <h2 class="text-xl font-semibold text-gray-900 dark:text-white">新規登録</h2>
-                        </div>
-                    </a>
-                    <p class="mt-2 text-xs text-gray-900 dark:text-white">※登録無料 / 広告なし</p>
-                    {{-- ログインボタン --}}
-                    <a href="{{ route('login') }}" class="w-full mt-4 scale-100 bg-white p-6 dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-md dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250">
-                        <div class="flex w-full items-center justify-center">
-                            <h2 class="text-xl font-semibold text-gray-900 dark:text-white">ログイン</h2>
-                        </div>
-                    </a>
-                    {{-- ゲストログインボタン --}}
-                    <a href="{{ route('guest.register') }}" class="mt-10 w-full scale-100 p-6 bg-gray-800/50  rounded-lg shadow-md flex motion-safe:hover:scale-[1.01] transition-all duration-250">
-                        <div class="flex w-full items-center justify-center">
-                            <h2 class="text-xl font-semibold text-white">ゲストログイン</h2>
-                        </div>
-                    </a>
-                    <p class="mt-2 text-xs text-gray-900 dark:text-white">※ゲストログイン中のデータは保存されません</p>
+            @guest
+                <div class="mt-16">
+                    <div class="flex flex-col items-center">
+                        {{-- 新規登録ボタン --}}
+                        <a href="{{ route('register') }}" class="w-full scale-100 bg-white p-6 dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-md dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250">
+                            <div class="flex w-full items-center justify-center">
+                                <h2 class="text-xl font-semibold text-gray-900 dark:text-white">新規登録</h2>
+                            </div>
+                        </a>
+                        <p class="mt-2 text-xs text-gray-900 dark:text-white">※登録無料 / 広告なし</p>
+                        {{-- ログインボタン --}}
+                        <a href="{{ route('login') }}" class="w-full mt-4 scale-100 bg-white p-6 dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-md dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250">
+                            <div class="flex w-full items-center justify-center">
+                                <h2 class="text-xl font-semibold text-gray-900 dark:text-white">ログイン</h2>
+                            </div>
+                        </a>
+                        {{-- ゲストログインボタン --}}
+                        <a href="{{ route('guest.register') }}" class="mt-10 w-full scale-100 p-6 bg-gray-800/50  rounded-lg shadow-md flex motion-safe:hover:scale-[1.01] transition-all duration-250">
+                            <div class="flex w-full items-center justify-center">
+                                <h2 class="text-xl font-semibold text-white">ゲストログイン</h2>
+                            </div>
+                        </a>
+                        <p class="mt-2 text-xs text-gray-900 dark:text-white">※ゲストログイン中のデータは保存されません</p>
+                    </div>
                 </div>
-            </div>
+            @endguest
         </div>
         {{-- Meesicの説明文 --}}
         <div class="w-full mt-8 px-6 py-40 flex flex-col items-center bg-white rounded-xl">
